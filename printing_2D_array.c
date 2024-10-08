f7a745d4e0f08be73ac1ae6cd4d25ee5be8d1147
Nikhil Lanje <nikhillanje00@gmail.com>
2024-10-08 11:49:38 -0700

    Add files via upload

---
+++ printing_2D_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int a[3][3],i,j;
+
+    printf("enter the elements of 3*3 matrics");
+
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");        
+        
+    }
+}
